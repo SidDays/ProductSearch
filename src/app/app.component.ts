f806771a57b48a7005d49bd135245c68a4d91205
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-03-27 15:49:30 -0700

    Bind all form fields with TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { freemem } from 'os';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,60 @@ export class AppComponent {
   public title: string = 'ProductSearch';
   public results = [];
 
-  public keywords = "";
+  // Form fields
+  public keywords: string = "";
+  public categoryId: number = -1;
+  public categories: Array<{ id: number, name: string }> = [
+    {
+      id: -1,
+      name: "All Categories"
+    },
+    {
+      id: 550,
+      name: "Art"
+    },
+    {
+      id: 2984,
+      name: "Baby"
+    },
+    {
+      id: 11450,
+      name: "Clothing, Shoes & Accessories"
+    },
+    {
+      id: 58058,
+      name: "Computers/Tablets & Networking"
+    },
+    {
+      id: 26395,
+      name: "Health & Beauty"
+    },
+    {
+      id: 11233,
+      name: "Music"
+    },
+    {
+      id: 1249,
+      name: "Video games & Consoles"
+    },
+  ];
+  public condition: { new: boolean, used: boolean, unspecified: boolean } = {
+    new: false,
+    used: false,
+    unspecified: false
+  };
+  public shippingOption: { local: boolean, free: boolean } = {
+    local: false,
+    free: false
+  }
+  public distance: number;
+  public from: { location: string, zipCode: number } = {
+    location: "currentLocation",
+    zipCode: null
+  }
 
   public callAPI() {
-    //  console.log("hello World");
-    console.log(this.keywords);
+    console.log("Form fields are", this.keywords, this.categoryId, this.condition, this.shippingOption, this.distance, this.from);
   }
 }
 
