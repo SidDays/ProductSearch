868b72e8032c61a1682c5ff07fa1c0735a3955f4
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-03-31 13:29:23 -0700

    Result and wish list pills (only displayed atm)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,6 +66,12 @@ export class AppComponent {
 
   public loadingResults: boolean = false;
   public results = null;
+
+  /** `true` if pill nav is on the Wish List, `false` if it is on Results. */
+  public wishList: boolean = false;
+  public pillActiveClass: string = "nav-link bg-dark text-white";
+  public pillInactiveClass: string = "nav-link text-body";
+
   public pageNumber: number = 1;
 
   /**
